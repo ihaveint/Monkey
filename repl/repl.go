f2a1e27b7e133c190e4ddbdec7e6ff1d2c8582fa
Soroosh <ihaveint@gmail.com>
2021-12-10 15:17:27 +0330

    WIP: Add Evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/ihaveint/monkey/evaluator"
 	"github.com/ihaveint/monkey/lexer"
 	"github.com/ihaveint/monkey/parser"
 	"io"
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
